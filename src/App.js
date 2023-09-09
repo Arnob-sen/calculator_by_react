665d0cde449e94742c9a34cae92f4392452d5bcb
Arnob-sen <arnobsen16.2@gmail.com>
2023-09-09 18:25:35 +0600

    give the ui and css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <form>
+        <input className='current-operand output' type={'text'}  />
+      </form>
+
+      <div className='buttons'>
+        <button className='ac span-two'  >AC</button>
+        <button onClick >Del</button>
+
+        <button id='divide' name='/'  >&divide;</button>
+        <button id='seven' name='7'  >7</button>
+        <button id='eight' name='8'  >8</button>
+        <button id='nine' name='9'  >9</button>
+        <button id='multiply' name='*'  >&times;</button>
+        <button id='four' name='4'  >4</button>
+        <button id='five' name='5'  >5</button>
+        <button id='six' name='6'  >6</button>
+        <button id='substract' name='-'  >-</button>
+        <button id='one' name='1'  >1</button>
+        <button id='two' name='2'  >2</button>
+        <button id='three' name='3'  >3</button>
+        <button id='add' name='+'  >+</button>
+        <button id='zero' name='0'  >0</button>
+        <button id='decimal' name='.'  >.</button>
+        <button id='equal' className='span-two' >=</button>
+      </div>
     </div>
-  );
+  
+  )
 }
 
 export default App;
