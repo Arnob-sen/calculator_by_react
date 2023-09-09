ef00ab0ad4dbaa799782cfc4bfdb16c425bcd590
Arnob-sen <arnobsen16.2@gmail.com>
2023-09-09 18:45:09 +0600

    give the functionality

--- src/App.js
+++ src/App.js
@@ -4,32 +4,56 @@ import { useState } from 'react';
 import React from 'react';
 
 function App() {
+  const [result,setResult]=useState('');
+  const handleClick=(e)=>{
+    setResult(result.concat(e.target.name));
+  }
+  const clear=()=>
+  {
+    setResult('');
+  }
+  const del=()=>
+  {
+    setResult(result.slice(0,-1));
+
+  }
+  const calculate=()=>
+  {
+    try {
+      setResult(eval(result).toString())
+      
+    } catch (error) {
+      setResult('syntax error');
+      
+    }
+
+  }
   return (
     <div className="App">
      <form>
-        <input className='current-operand output' type={'text'}  />
+        <input className='current-operand output' type={'text'} value={result}  />
       </form>
 
       <div className='buttons'>
-        <button className='ac span-two'  >AC</button>
-        <button onClick >Del</button>
+        <button id='clear' className='ac span-two' onClick={clear}  >AC</button>
+        <button  onClick ={del}>Del</button>
 
-        <button id='divide' name='/'  >&divide;</button>
-        <button id='seven' name='7'  >7</button>
-        <button id='eight' name='8'  >8</button>
-        <button id='nine' name='9'  >9</button>
-        <button id='multiply' name='*'  >&times;</button>
-        <button id='four' name='4'  >4</button>
-        <button id='five' name='5'  >5</button>
-        <button id='six' name='6'  >6</button>
-        <button id='substract' name='-'  >-</button>
-        <button id='one' name='1'  >1</button>
-        <button id='two' name='2'  >2</button>
-        <button id='three' name='3'  >3</button>
-        <button id='add' name='+'  >+</button>
-        <button id='zero' name='0'  >0</button>
-        <button id='decimal' name='.'  >.</button>
-        <button id='equal' className='span-two' >=</button>
+        <button id='divide' name='/' onClick={handleClick}  >&divide;</button>
+        <button id='seven' name='7'  onClick={handleClick} >7</button>
+        <button id='eight' name='8' onClick={handleClick} >8</button>
+        <button id='nine' name='9' onClick={handleClick} >9</button>
+        <button id='multiply' name='*' onClick={handleClick} >&times;</button>
+        <button id='four' name='4' onClick={handleClick} >4</button>
+        <button id='five' name='5' onClick={handleClick} >5</button>
+        <button id='six' name='6' onClick={handleClick} >6</button>
+        <button id='substract' name='-' onClick={handleClick} >-</button>
+        <button id='one' name='1' onClick={handleClick} >1</button>
+        <button id='two' name='2' onClick={handleClick} >2</button>
+        <button id='three' name='3' onClick={handleClick} >3</button>
+        <button id='add' name='+' onClick={handleClick} >+</button>
+        <button id='zero' name='0' onClick={handleClick} >0</button>
+        <button id='decimal' name='.' onClick={handleClick} >.</button>
+        <button id='equal' className='span-two' onClick={calculate} >=</button>
       </div>
     </div>
   
